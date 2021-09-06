6e6d6ad0ad6331b4a0e41048f33e6e62824d4eed
Sweipe <56695050+Sweipe@users.noreply.github.com>
2021-09-06 15:35:00 +0200

    Add files via upload

---
+++ baskunskapervariabler/baskunskapervariabler/Classes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Sphere
+{
+    public int radius = 5;
+    public string color = "none";
+    public Sphere(string Color)
+    {
+        color = Color;
+    }
+
+    public void ShoutColor()
+    {
+        Console.WriteLine("My color is {0}", color);
+    }
+}
