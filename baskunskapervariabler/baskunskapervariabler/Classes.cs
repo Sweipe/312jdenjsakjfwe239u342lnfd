20c65626fa4af779e6ee14f4cb0543d3de6c69e7
Sweipe <56695050+Sweipe@users.noreply.github.com>
2021-09-06 15:49:43 +0200

    Add files via upload
    Uploaded the whole thing, .exe and not .exe. Many unnecessary stuff e

---
+++ baskunskapervariabler/baskunskapervariabler/Classes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Sphere
+{
+    //The variables of this class, all 'Sphere' objects will have these
+    public int radius = 5;
+    public string color = "none";
+    public Sphere(string Color) //the 'Sphere' object will be declared with a string argument
+    {
+        color = Color;
+    }
+
+    //This down here is a method
+    public void ShoutColor()
+    {
+        Console.WriteLine("My color is {0}", color);
+    }
+}
---
+++ baskunskapervariabler/baskunskapervariabler/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace baskunskapervariabler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A few variables
+            var anything = 32;
+            var secondthing = "hero orl";
+            int num = 32;
+            int sum = num + 1;
+            string anyText = "hello word";
+            float floatie = 1.234567f;
+            char character = 'a';
+
+            //Declaring objects will class 'Sphere' and their unique string arguments
+            Sphere redball = new Sphere("red");
+            var oddball = new Sphere("gray");
+
+            //calling method from the class 'Sphere'
+            redball.ShoutColor();
+            oddball.ShoutColor();
+
+            //this will store user input in a file called 'something.txt'
+            Console.Write("Comment: ");
+            File.AppendAllText("something.txt", Console.ReadLine() + "\n");
+
+            //this checks for file named 'activation.txt'
+            //Registers first time the program is executed
+            if (!File.Exists("activation.txt")) {
+                File.AppendAllText("activation.txt", DateTime.Now.ToString());
+            }
+        }
+    }
+
+}
