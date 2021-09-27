e88546040fed770018721a0736ca726b81259f72
Sweipe <56695050+Sweipe@users.noreply.github.com>
2021-09-27 11:34:08 +0200

    Add files via upload

---
+++ ScopesAndModifiers/Classes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class MasterClass
+{
+    public string pu = "Public.";
+    protected string pro = "Protected.";
+    private string pri = "Private.";
+
+    public void Publico()
+    {
+        Console.WriteLine(pu);
+    }
+    public void Protecto()
+    {
+        Console.WriteLine(pro);
+    }
+    public void Privato()
+    {
+        Console.WriteLine(pri);
+    }
+
+}
+
+public class ChildClass : MasterClass
+{
+    public void NewPublico()
+    {
+        Console.WriteLine(pu);
+    }
+    public void NewProtecto()
+    {
+        Console.WriteLine(pro);
+    }
+}
---
+++ ScopesAndModifiers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScopesAndModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MasterClass myObj = new MasterClass();
+            myObj.Publico();
+            myObj.Protecto();
+            myObj.Privato();
+            Console.WriteLine();
+
+            ChildClass childObj = new ChildClass();
+            childObj.NewPublico();
+            childObj.NewProtecto();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
