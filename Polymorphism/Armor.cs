7295730409f84a5163aa354485b9fb512e6b971b
Sweipe <56695050+Sweipe@users.noreply.github.com>
2021-10-25 10:57:49 +0200

    Add files via upload

---
+++ Polymorphism/Armor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Armor
+    {
+        public string type = "generic";
+        public Armor()
+        {
+            Report();
+        }
+        public virtual void Report() {
+            Console.WriteLine("Armor");
+        }
+    }
+    public class APC : Armor {
+        //public string type = "APC";
+        public override void Report() {
+            Console.WriteLine("APC Armor");
+            type = "apc";
+        }
+    }
+    public class IFV : Armor {
+        public override void Report()
+        {
+            Console.WriteLine("IFV Armor");
+        }
+    }
+
+    public class Tank {
+        public string type = "tank";
+        public Tank() {
+            Report();
+        }
+        public virtual void Report()
+        {
+            Console.WriteLine("Base tank");
+        }
+    }
+
+    public class M1 : Tank {
+        public string name = "M1";
+        public override void Report()
+        {
+            Console.WriteLine("Sub tank");
+        }
+    }
+
+    public class M1A2 : M1 {
+        public string version = "A2";
+        public override void Report()
+        {
+            Console.WriteLine("SubSub tank");
+        }
+    }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Armor armor = new Armor();
+            Armor apc = new APC();
+            Console.WriteLine(apc.type);
+            Armor ifv = new IFV();
+
+            Console.WriteLine();//Separator
+
+            Tank tank = new Tank();
+            Tank m1 = new M1();
+            Tank m1a2 = new M1A2();
+
+            Console.ReadKey();
+        }
+    }
+}
