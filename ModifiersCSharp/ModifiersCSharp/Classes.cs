b6d7922779d392193e99898a3058bfa415a04be7
Sweipe <56695050+Sweipe@users.noreply.github.com>
2021-11-16 20:19:21 +0100

    Add files via upload

---
+++ ModifiersCSharp/ModifiersCSharp/Classes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ModifiersCSharp
+{
+    class Parent
+    {
+        public string name;                 //public: Kan anropas utanför klassen
+        protected string type = "human";    //protected: Kan anropas i sin klass eller i en subklass
+        private int money = 50000;          //private: Kan bara anropas i sin klass
+
+        public Parent(string name)
+        {
+            this.name = name;
+        }
+    }
+
+    class Child : Parent
+    {
+        public Child(string name) : base(name)
+        {}
+        public void SayName() {
+            Console.WriteLine(name);    //Anropar offentlig klass funkar bra
+        }
+        public void SayType() {
+            Console.WriteLine(type);    //Anropar skyddad klass funkar bra
+        }
+        public void SayMoney() {
+            Console.WriteLine(money);   //Denna subklass försöker anropa en privat variabel från sin superklass, alltså (en annan klass), vilket inte går
+        }
+    }
+}
---
+++ ModifiersCSharp/ModifiersCSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ModifiersCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Child childObj = new Child("Victor");
+            Console.WriteLine(childObj.name);       //Anropning av offentlig fält utanför sin klass funkar bra
+            Console.WriteLine(childObj.type);       //Anropning av skyddad fält utanför sin klass stöter på problem
+            Console.WriteLine(childObj.money);      //Anropning av privat fält utanför sin klass stöter på problem
+
+            //Funktioner som offentlig-gör skyddade och private fält/variabler för att kringgå modifieraren.
+            childObj.SayName();
+            childObj.SayType();
+            childObj.SayMoney();
+
+            Console.ReadKey();
+        }
+    }
+}
