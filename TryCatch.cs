a1e286b43816490c24a6dfb442a0213a29e2aacd
Sweipe <56695050+Sweipe@users.noreply.github.com>
2022-01-19 11:15:39 +0100

    Add files via upload

---
+++ TryCatch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Secure();
+            Insecure();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+            void Secure()
+            {
+                try
+                {
+                    Console.Write("(Säker)  Skriv in ett tal som talet 10 kan divideras med:");
+                    int num = 10/int.Parse(Console.ReadLine());
+                    Console.WriteLine($"Felfritt, resultatet blev {num}");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Ett fel uppstod med ditt tal");
+                }
+            }
+
+            void Insecure()
+            {
+                Console.Write("(Osäker) Skriv in ett tal som talet 10 kan divideras med:");
+                int num = 10/int.Parse(Console.ReadLine());
+                Console.WriteLine($"Felfritt, resultatet blev {num}");
+            }
+        }
+    }
+}
